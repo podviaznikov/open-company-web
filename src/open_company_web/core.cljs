1bd4e0d5a1d512fa99901149109720ec047b5d43
bago2k4 <iacopo@rc0.eu>
2015-11-02 22:55:27 +0100

    add all the routes with trailing slash, remove history stuff, initialize accountant with secretary passing the dispatcher and the list of routes

--- src/open_company_web/core.cljs
+++ src/open_company_web/core.cljs
@@ -2,7 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [secretary.core :as secretary :refer-macros [defroute]]
             [open-company-web.router :as router]
-            [open-company-web.components.page :refer [company]]
+            [open-company-web.components.page :refer [company company-profile-container]]
             [open-company-web.components.list-companies :refer [list-companies]]
             [open-company-web.components.page-not-found :refer [page-not-found]]
             [open-company-web.components.user-profile :refer [user-profile]]
@@ -14,7 +14,8 @@
             [goog.events :as events]
             [open-company-web.lib.cookies :as cook]
             [open-company-web.local-settings :as ls]
-            [open-company-web.lib.jwt :as jwt])
+            [open-company-web.lib.jwt :as jwt]
+            [accountant.core :as accountant])
   (:import [goog.history EventType]))
 
 (enable-console-print!)
@@ -27,7 +28,7 @@
 (if-let [target (. js/document (getElementById "app"))]
   (do
 
-    (defroute login-route "/login" {:keys [query-params]}
+    (defn login-routes-handler [query-params]
       (if (contains? query-params :jwt)
         (do ; contains :jwt so auth went well
           (cook/set-cookie! :jwt (:jwt query-params) (* 60 60 24 60) "/" ls/jwt-cookie-domain ls/jwt-cookie-secure)
@@ -54,7 +55,21 @@
           ; render component
           (om/root login app-state {:target target}))))
 
-    (defn home-handler []
+    (defroute login-route ((:login router/routes) 0) {:keys [query-params]}
+      (login-routes-handler query-params))
+
+    (defroute login-route-slash ((:login router/routes) 1) {:keys [query-params]}
+      (login-routes-handler query-params))
+
+    (defn login-wall []
+      (let [token (router/get-token)]
+        (when-not (.startsWith token "/login")
+          (if (cook/get-cookie :jwt)
+            true
+            (utils/redirect! (str "/login?login-redirect=" (router/get-token)))))))
+
+    (defn home-routes-handler []
+      (login-wall)
       ; save route
       (router/set-route! ["companies"] {})
       ; load data from api
@@ -62,30 +77,38 @@
       ; render component
       (om/root list-companies app-state {:target target}))
 
-    (defroute home-page-route-slash "/" []
-      (home-handler))
+    (defroute home-page-route ((:home router/routes) 0) []
+      (home-routes-handler))
 
-    (defroute home-page-route "/" []
-      (home-handler))
+    (defroute home-page-route-slash ((:home router/routes) 1) []
+      (home-routes-handler))
 
-    (defroute list-page-route-slash "/companies/" []
-      (home-handler))
+    (defroute list-page-route ((:home router/routes) 2) []
+      (home-routes-handler))
 
-    (defroute list-page-route "/companies" []
-      (home-handler))
+    (defroute list-page-route-slash ((:home router/routes) 3) []
+      (home-routes-handler))
 
-    (defroute company-profile-route "/companies/:slug/profile" {:as params}
+    (defn company-profile-routes-handler [params]
+      (login-wall)
       (let [slug (:slug (:params params))
             query-params (:query-params params)]
         ; save route
-        (router/set-route! ["companies" slug] {:slug slug :query-params query-params})
+        (router/set-route! ["companies" slug "profile"] {:slug slug :query-params query-params})
         ; load data from api
         (api/get-company slug)
         (swap! app-state assoc :loading true)
         ; render compoenent
-        (om/root company app-state {:target target})))
+        (om/root company-profile-container app-state {:target target})))
 
-    (defroute company-route "/companies/:slug" {:as params}
+    (defroute company-profile-route ((:company-profile router/routes) 0) {:as params}
+      (company-profile-routes-handler params))
+
+    (defroute company-profile-route-slash ((:company-profile router/routes) 1) {:as params}
+      (company-profile-routes-handler params))
+
+    (defn company-routes-handler [params]
+      (login-wall)
       (let [slug (:slug (:params params))
             query-params (:query-params params)]
         ; save route
@@ -98,16 +121,14 @@
         ; render compoenent
         (om/root company app-state {:target target})))
 
-    (defn section-handler [slug section]
-      ; if there are no company data
-      (when-not (contains? @app-state (keyword slug))
-        ; load data from api
-        (swap! app-state assoc :loading true)
-        (api/get-company slug))
-      ; render component
-      (om/root company app-state {:target target}))
+    (defroute company-route ((:company router/routes) 0) {:as params}
+      (company-routes-handler params))
 
-    (defroute section-route "/companies/:slug/:section" {:as params}
+    (defroute company-route-slash ((:company router/routes) 1) {:as params}
+      (company-routes-handler params))
+
+    (defn section-routes-handler [params]
+      (login-wall)
       (let [slug (:slug (:params params))
             section (:section (:params params))
             query-params (:query-params params)]
@@ -115,53 +136,63 @@
         (router/set-route! ["companies" slug section] {:slug slug
                                                        :section section
                                                        :query-params query-params})
-        (section-handler slug section)))
+        ; if there are no company data
+        (when-not (contains? @app-state (keyword slug))
+          ; load data from api
+          (swap! app-state assoc :loading true)
+          (api/get-company slug))
+        ; render component
+        (om/root company app-state {:target target})))
 
-    (defroute user-profile-route "/profile" {:as params}
+    (defroute section-route ((:section router/routes) 0) {:as params}
+      (section-routes-handler params))
+
+    (defroute section-route-slash ((:section router/routes) 1) {:as params}
+      (section-routes-handler params))
+
+    (defn user-profile-routes-handler [params]
+      (login-wall)
       (om/root user-profile app-state {:target target}))
 
+    (defroute user-profile-route ((:profile router/routes) 0) {:as params}
+      (user-profile-routes-handler params))
+
+    (defroute user-profile-route-slash ((:profile router/routes) 1) {:as params}
+      (user-profile-routes-handler params))
+
     (defroute not-found-route "*" []
-      ; render component
-      (om/root page-not-found app-state {:target target}))
+      (do
+        (login-wall)
+        ; render component
+        (om/root page-not-found app-state {:target target})))
+
+    (def routes-stack
+      [login-route
+       login-route-slash
+       home-page-route
+       home-page-route-slash
+       list-page-route
+       list-page-route-slash
+       company-route
+       company-route-slash
+       company-profile-route
+       company-profile-route-slash
+       section-route
+       section-route-slash
+       user-profile-route
+       user-profile-route-slash
+       not-found-route])
 
     (def dispatch!
-      (secretary/uri-dispatcher [login-route
-                                 home-page-route-slash
-                                 home-page-route
-                                 list-page-route-slash
-                                 list-page-route
-                                 company-route
-                                 company-profile-route
-                                 section-route
-                                 user-profile-route
-                                 not-found-route]))
+      (secretary/uri-dispatcher routes-stack))
 
-    (defn login-wall []
-      (let [token (router/get-token)]
-        (when-not (.startsWith token "/login")
-          (if (cook/get-cookie :jwt)
-            true
-            (utils/redirect! (str "/login?login-redirect=" (router/get-token)))))))
+    (def routes-list
+      (vec (flatten (map #(% router/routes) (keys router/routes)))))
 
-    (defn handle-url-change [e]
-      ;; we are checking if this event is due to user action,
-      ;; such as click a link, a back button, etc.
-      ;; as opposed to programmatically setting the URL with the API
-      (when-not (.-isNavigation e)
-        ;; in this case, we're setting it so
-        ;; let's scroll to the top to simulate a navigation
-        (js/window.scrollTo 0 0))
-      ; check if the user is logged in
-      (login-wall)
-      ;; dispatch on the token
-      (dispatch! (router/get-token)))))
-
-(defonce history
-  (doto (router/make-history)
-    (events/listen EventType.NAVIGATE
-      ;; wrap in a fn to allow live reloading
-      #(handle-url-change %))
-    (.setEnabled true)))
+    ; initialize accountant to handle token changes and link clicks
+    (accountant/configure-navigation! dispatch! routes-list)
+    ; dispatche the token
+    (dispatch! (router/get-token))))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
